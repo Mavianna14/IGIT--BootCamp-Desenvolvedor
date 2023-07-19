d9ec1504b25f27c05015c6a121771bc1c9de2051
Marlon Vianna <marlonvianna@yahoo.com.br>
2023-07-19 09:57:06 -0300

    JavaScript

---
+++ JavaScript/aula1.js
@@ -0,0 +1,14 @@
+
+
+var mensagem = 'Olá';
+
+mensagem ='Olá Marlon';
+
+console.log (mensagem);
+
+// Comentário feito na linha// 
+
+/* 
+bloco de comentário. Nada aqui dentro vai ser processado
+*/
+
