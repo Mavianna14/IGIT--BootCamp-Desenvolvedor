971649350294fbf34bd21942b96d9ad9a07901a0
Marlon Vianna <marlonvianna@yahoo.com.br>
2023-08-01 17:55:33 -0300

    Comando-repetição

---
+++ JavaScript/comando-repeticao.js
@@ -0,0 +1,28 @@
+
+
+console.log ("Imprimindo de 1 a 5 com While")
+
+var contador1 = 1;
+while(contador1 <=5) {
+    console.log (contador1);
+    contador1++;
+}
+
+
+console.log ("Imprimindo de 1 a 5 com Do While")
+
+var contador2 = 1;
+do{
+    console.log(contador2);
+    contador2++;   
+} while(contador2 <= 5)
+
+
+
+
+console.log ("Imprimindo de 1 a 5 com For")
+
+for ( var contador3 =1; contador3 <= 5; contador3++){
+    console.log(contador3);
+}
+
