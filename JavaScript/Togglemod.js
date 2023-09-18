71f3f489d3a3522f3e8885c4f1f9e684cc97523b
Marlon Vianna <marlonvianna@yahoo.com.br>
2023-09-18 15:07:25 -0300

    Material-alterado

--- JavaScript/Togglemod.js
+++ JavaScript/Togglemod.js
@@ -8,3 +8,23 @@ function toggleMode(){
         html.classList.add('light')
     }
 }
+
+//Como trocar a imagem, quando trocar o Light para dark mode
+
+
+//pegar a tag img 
+const img = document.querySelector("#profile img")
+
+// O #profile img é o id feito no htlm
+
+// subistituir a img 
+
+if( html.classList.contains("light")) {
+
+    //se tiver com o light mode ligado, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+
+} else {
+    // se estiver com o light mode desativado, manter a foto original
+    img.setAttribute("src", ./assets/avatar.png)
+}
